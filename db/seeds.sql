64ffa91c2944197c6e450a3ed1fe4c423f8ff407
Anne <coloradoz99@gmail.com>
2018-03-11 20:40:02 -0600

    updates todeployed database

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,9 +6,4 @@ INSERT INTO burgers (name, devoured) VALUES ('South Western Cheese', true);
 INSERT INTO burgers (name, devoured) VALUES ('Meat Lover', true);
 INSERT INTO burgers (name, devoured) VALUES ('Vegan', true);
 INSERT INTO burgers (name) VALUES ('Philly Cheese');
-INSERT INTO burgers (name) VALUES ('Big Mac');
-INSERT INTO burgers (name) VALUES ('BBQ Bacon');
-INSERT INTO burgers (name, devoured) VALUES ('South Western Cheese', true);
-INSERT INTO burgers (name, devoured) VALUES ('Meat Lover', true);
-INSERT INTO burgers (name, devoured) VALUES ('Vegan', true);
-INSERT INTO burgers (name) VALUES ('Philly Cheese');
+
