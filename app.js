e8f043530401c77b370120ce166d247bdb594908
AR123456 <coloradoz99@gmail.com>
2022-10-30 11:17:10 -0600

    getting starting set up for loan calculator

---
+++ app.js
@@ -0,0 +1,9 @@
+// TODO create a app that will take in a dollar amount goal an years goal
+// the app will calculate how much needs to be saved per month to achive the goal
+// use an assumed IRR of 8% then make that dynamic
+//
+// IRR on investopedia  https://www.investopedia.com/terms/i/irr.asp
+//The internal rate of return (IRR) is the annual rate of growth that an investment is expected to generate.
+//IRR is calculated using the same concept as net present value (NPV), except it sets the NPV equal to zero.   IRR can help investors determine the investment return of various assets
+
+//JS Math metohds    https://www.w3schools.com/js/js_math.asp
