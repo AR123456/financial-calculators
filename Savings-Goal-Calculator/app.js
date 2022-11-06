0ed4a2fc9216427c9a700be2c92dbcce14015c98
AR123456 <coloradoz99@gmail.com>
2022-11-06 09:05:03 -0700

    getting range value on page

--- Savings-Goal-Calculator/app.js
+++ Savings-Goal-Calculator/app.js
@@ -2,3 +2,12 @@
 // the app will calculate how much needs to be saved per month to achive the goal
 // use an assumed IRR of 8% then make that dynamic
 //
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
