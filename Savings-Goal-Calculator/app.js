ba0bdb0a7facbcdff6fe0b47078d7f6509a77dc4
AR123456 <coloradoz99@gmail.com>
2022-11-06 09:05:25 -0700

    adding other ranges

--- Savings-Goal-Calculator/app.js
+++ Savings-Goal-Calculator/app.js
@@ -3,11 +3,12 @@
 // use an assumed IRR of 8% then make that dynamic
 //
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+const savingsRange = document.getElementById("savingsRange");
+const savingsGoal = document.getElementById("savingsGoal");
+savingsGoal.innerHTML = savingsRange.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-  output.innerHTML = this.value;
+// TODO update the value based on slider or typing in text box
+savingsRange.oninput = function () {
+  savingsGoal.innerHTML = this.value;
 };
