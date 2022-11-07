627f2d3cd503450f2a92fa1350909735b1a66258
AR123456 <coloradoz99@gmail.com>
2022-11-07 06:15:09 -0700

    not working export

--- Savings-Goal-Calculator/app.js
+++ Savings-Goal-Calculator/app.js
@@ -2,8 +2,7 @@
 // the app will calculate how much needs to be saved per month to achive the goal
 // use an assumed IRR of 8% then make that dynamic
 //
-// imports
-import { myChart } from "./planChart";
+
 ///
 const ctx = document.getElementById("myChart");
 const savingsRange = document.getElementById("savingsRange");
@@ -15,4 +14,44 @@ savingsGoal.innerHTML = savingsRange.value; // Display the default slider value
 savingsRange.oninput = function () {
   savingsGoal.innerHTML = this.value;
 };
-myChart(ctx);
+// CHART BOILER PLATE
+
+// https://www.chartjs.org/docs/latest/getting-started/usage.html
+
+const ctx = document.getElementById("myChart");
+export const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
