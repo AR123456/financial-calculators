b37649f5b29bb7780e229472e45a0f8cba1e5f06
AR123456 <coloradoz99@gmail.com>
2022-11-07 06:14:21 -0700

    trying import export

--- Savings-Goal-Calculator/app.js
+++ Savings-Goal-Calculator/app.js
@@ -2,7 +2,10 @@
 // the app will calculate how much needs to be saved per month to achive the goal
 // use an assumed IRR of 8% then make that dynamic
 //
-
+// imports
+import { myChart } from "./planChart";
+///
+const ctx = document.getElementById("myChart");
 const savingsRange = document.getElementById("savingsRange");
 const savingsGoal = document.getElementById("savingsGoal");
 savingsGoal.innerHTML = savingsRange.value; // Display the default slider value
@@ -12,106 +15,4 @@ savingsGoal.innerHTML = savingsRange.value; // Display the default slider value
 savingsRange.oninput = function () {
   savingsGoal.innerHTML = this.value;
 };
-
-// CHART BOILER PLATE
-
-// https://www.chartjs.org/docs/latest/getting-started/usage.html
-
-const ctx = document.getElementById("myChart");
-const myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
-
-//////////// https://www.chartjs.org/docs/latest/samples/bar/border-radius.html
-
-const config = {
-  type: "bar",
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: "Chart.js Bar Chart",
-      },
-    },
-  },
-};
-const DATA_COUNT = 7;
-const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
-
-const labels = Utils.months({ count: 7 });
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Fully Rounded",
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.red,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-      borderWidth: 2,
-      borderRadius: Number.MAX_VALUE,
-      borderSkipped: false,
-    },
-    {
-      label: "Small Radius",
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.blue,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-      borderWidth: 2,
-      borderRadius: 5,
-      borderSkipped: false,
-    },
-  ],
-};
-const actions = [
-  {
-    name: "Randomize",
-    handler(chart) {
-      chart.data.datasets.forEach((dataset) => {
-        dataset.data = Utils.numbers({
-          count: chart.data.labels.length,
-          min: -100,
-          max: 100,
-        });
-      });
-      chart.update();
-    },
-  },
-];
+myChart(ctx);
