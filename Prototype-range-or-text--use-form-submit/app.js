522ff585d94ae203de09c1a0ab2cc9b9e8308b3b
AR123456 <coloradoz99@gmail.com>
2022-11-25 09:18:51 -0700

    extend prototype to use form submit

---
+++ Prototype-range-or-text--use-form-submit/app.js
@@ -0,0 +1,48 @@
+// vars
+const calculateButton = document.getElementById("calculate");
+// range slider
+const sliderSavingsGoal = document.getElementById("savingsRange");
+//input box
+const inputGoal = document.getElementById("savingsInput");
+
+//////////////////////////// method one
+
+// function setup() {
+//   // https://stackoverflow.com/questions/64199456/changing-the-value-of-the-range-slider-and-input-box-at-the-same-time
+
+//   sliderSavingsGoal.addEventListener("input", function () {
+//     inputGoal.value = this.value;
+//   });
+//   inputGoal.addEventListener("input", function () {
+//     sliderSavingsGoal.value = this.value;
+//   });
+//   goal = parseFloat(inputGoal.value);
+//   console.log(sliderSavingsGoal.value);
+//   console.log(inputGoal.value);
+//   console.log(goal);
+// }
+
+// setup();
+
+calculateButton.onclick = function () {
+  // since the range and input box have been synced can just get the value from one of them, so using box
+  // https://www.youtube.com/watch?v=iKo9pDKKHnc&t=25s 17:20 in
+  goal = inputGoal.value;
+  console.log(goal);
+  // setup();
+  // calculate();
+  // syncSavingsGoal();
+};
+//
+
+// ////////////////////////// alternative way to get values link slider up to number input Kyle Web Dev
+// https://www.youtube.com/watch?v=iKo9pDKKHnc&t=25s
+// any time input changes call this function - this syncs the slider and box
+inputGoal.addEventListener("input", syncSavingsGoal);
+sliderSavingsGoal.addEventListener("input", syncSavingsGoal);
+function syncSavingsGoal(e) {
+  const value = e.target.value;
+  inputGoal.value = value;
+  sliderSavingsGoal.value = value;
+}
+// get the value from the inputs
--- Prototype-range-or-text-box/app.js
+++ Prototype-range-or-text-box/app.js
@@ -1,6 +1,8 @@
 // vars
 const calculateButton = document.getElementById("calculate");
+// range slider
 const sliderGoal = document.getElementById("savingsRange");
+//input box
 const inputGoal = document.getElementById("savingsRangeInput");
 
 function setup() {
@@ -18,9 +20,27 @@ function setup() {
   console.log(goal);
 }
 
-setup();
+// setup();
 
 calculateButton.onclick = function () {
-  setup();
+  // since the range and input box have been synced can just get the value from one of them, so using box
+  // https://www.youtube.com/watch?v=iKo9pDKKHnc&t=25s 17:20 in
+  goal = inputGoal.value;
+  console.log(goal);
+  // setup();
   // calculate();
+  // syncSavingsGoal();
 };
+//
+
+// // alternative way to get values link slider up to number input Kyle Web Dev
+// https://www.youtube.com/watch?v=iKo9pDKKHnc&t=25s
+// any time input changes call this function - this syncs the slider and box
+inputGoal.addEventListener("input", syncSavingsGoal);
+sliderGoal.addEventListener("input", syncSavingsGoal);
+function syncSavingsGoal(e) {
+  const value = e.target.value;
+  inputGoal.value = value;
+  sliderGoal.value = value;
+}
+// get the value from the inputs
