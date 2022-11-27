d526a5a5ba9de1e808ab4e56cf4e94fd4c841e29
AR123456 <coloradoz99@gmail.com>
2022-11-27 05:11:34 -0700

    range or text input 2 ways

--- Prototype-range-or-text-adding-more-inputs/app.js
+++ Prototype-range-or-text-adding-more-inputs/app.js
@@ -1,44 +1,25 @@
-// imports
-// import { setup } from "./syncUsingOneFunction.js";
-
 // vars
 const calculateButton = document.getElementById("calculate");
 // range slider
 const sliderGoal = document.getElementById("savingsRange");
 //input box
-const inputGoal = document.getElementById("savingsInput");
-// // range slider
-// const sliderYears = document.getElementById("yearsRange");
-// //input box
-// const inputYears = document.getElementById("yearsInput");
+const inputGoal = document.getElementById("savingsRangeInput");
 
 //////////////////////////// method one
 
 function setup() {
   // https://stackoverflow.com/questions/64199456/changing-the-value-of-the-range-slider-and-input-box-at-the-same-time
-  ///////////// goal
+
   sliderGoal.addEventListener("input", function () {
     inputGoal.value = this.value;
   });
   inputGoal.addEventListener("input", function () {
     sliderGoal.value = this.value;
   });
-  goal = inputGoal.value;
+  goal = parseFloat(inputGoal.value);
   console.log(sliderGoal.value);
   console.log(inputGoal.value);
   console.log(goal);
-  //
-
-  // sliderYears.addEventListener("input", function () {
-  //   inputYears.value = this.value;
-  // });
-  // inputYears.addEventListener("input", function () {
-  //   sliderYears.value = this.value;
-  // });
-  // years = parseFloat(inputYears.value);
-  // console.log(sliderYears.value);
-  // console.log(inputYears.value);
-  // console.log(years);
 }
 
 setup();
@@ -47,36 +28,21 @@ calculateButton.onclick = function () {
   // since the range and input box have been synced can just get the value from one of them, so using box
   // https://www.youtube.com/watch?v=iKo9pDKKHnc&t=25s 17:20 in
   goal = parseFloat(inputGoal.value);
-  // years = inputYears.value;
   console.log(goal);
-  // console.log(years);
   // setup();
   // calculate();
+  // syncSavingsGoal();
 };
 //
 
-// // ////////////////////////// alternative way to get values link slider up to number input Kyle Web Dev
-// // https://www.youtube.com/watch?v=iKo9pDKKHnc&t=25s
-// // any time input changes call this function - this syncs the slider and box
+// ////////////////////////// alternative way to get values link slider up to number input Kyle Web Dev
+// https://www.youtube.com/watch?v=iKo9pDKKHnc&t=25s
+// any time input changes call this function - this syncs the slider and box
 // inputGoal.addEventListener("input", syncSavingsGoal);
-// sliderSavingsGoal.addEventListener("input", syncSavingsGoal);
+// sliderGoal.addEventListener("input", syncSavingsGoal);
 // function syncSavingsGoal(e) {
 //   const value = e.target.value;
 //   inputGoal.value = value;
-//   sliderSavingsGoal.value = value;
-// }
-// inputYears.addEventListener("input", syncYears);
-// sliderYears.addEventListener("input", syncYears);
-// function syncYears(e) {
-//   const value = e.target.value;
-//   inputYears.value = value;
-//   sliderYears.value = value;
+//   sliderGoal.value = value;
 // }
 // // get the value from the inputs
-// //TODO can this be DRY ? am taking in the event so have e
-// function syncInputAndRange(e) {
-//   console.log(`e.target.id = ${e.target.id}`);
-//   console.log(`e.target.value = ${e.target.value}`);
-//   console.log(e);
-//   //
-// }
