7d3bc09cfb7968d2dd3b0d99bc98a56e8486c4e4
AR123456 <coloradoz99@gmail.com>
2022-11-30 04:40:06 -0700

    clean up

--- HTML-Range-Input-With-Ruler/app.js
+++
@@ -1,8 +0,0 @@
-// https://www.youtube.com/watch?v=EomWt4Dc5xg
-document.documentElement.classList.add("js");
-
-addEventListener("input", (e) => {
-  let _t = e.target;
-
-  _t.parentNode.style.setProperty("--val", +_t.value);
-});
