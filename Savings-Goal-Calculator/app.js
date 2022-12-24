b101b5be581097c3ed055cc6be2c099d146bc192
AR123456 <coloradoz99@gmail.com>
2022-12-24 11:35:51 -0700

    working on range slider

--- Savings-Goal-Calculator/app.js
+++ Savings-Goal-Calculator/app.js
@@ -18,66 +18,45 @@ const actualTime = document.getElementById("actualTime");
 const calculateButton = document.getElementById("calculate");
 const viewReportButton = document.getElementById("viewReport");
 
-// Function to format input boxes as currency
-// the initial values, any changes and slider inputs should all be formated
-// target values on page load or with a change call this function
-function formatCurrency(num) {
-  console.log(
-    new Intl.NumberFormat("en-IN", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(num)
-  );
-}
-formatCurrency(inputGoal.value);
 // function to sync range and  text box inputs
 function syncInputs() {
   // https://stackoverflow.com/questions/64199456/changing-the-value-of-the-range-slider-and-input-box-at-the-same-time
 
-  // /////////////////////////
   sliderGoal.addEventListener("input", function () {
     inputGoal.value = this.value;
   });
   inputGoal.addEventListener("input", function () {
     sliderGoal.value = this.value;
-    // console.log(sliderGoal.dataset);
-    updateProgress(sliderGoal);
   });
   sliderYears.addEventListener("input", function () {
     inputYears.value = this.value;
   });
   inputYears.addEventListener("input", function () {
     sliderYears.value = this.value;
-    updateProgress(sliderYears);
   });
   sliderCurrentSaved.addEventListener("input", function () {
     inputCurrentSaved.value = this.value;
   });
   inputCurrentSaved.addEventListener("input", function () {
     sliderCurrentSaved.value = this.value;
-    updateProgress(sliderCurrentSaved);
   });
   sliderMonthlySavings.addEventListener("input", function () {
     inputMonthlySavings.value = this.value;
   });
   inputMonthlySavings.addEventListener("input", function () {
     sliderMonthlySavings.value = this.value;
-    updateProgress(sliderMonthlySavings);
   });
   sliderExpectedRate.addEventListener("input", function () {
     inputExpectedRate.value = this.value;
   });
   inputExpectedRate.addEventListener("input", function () {
     sliderExpectedRate.value = this.value;
-    updateProgress(sliderExpectedRate);
   });
   sliderInflationRate.addEventListener("input", function () {
     inputInflationRate.value = this.value;
   });
   inputInflationRate.addEventListener("input", function () {
     sliderInflationRate.value = this.value;
-    updateProgress(sliderInflationRate);
   });
 }
 syncInputs();
@@ -96,22 +75,14 @@ const calculate = () => {
   console.log(expectedReturn);
   console.log(expectedInflation);
   // real interest rate = interest rate - inflation rate
-  // removing for now
-  // const realInt = (expectedReturn - expectedInflation) / 100;
-  const realInt = expectedReturn / 100;
+  const realInt = (expectedReturn - expectedInflation) / 100;
   console.log(realInt);
-  calcTime =
-    Math.log(1 + (goal * realInt) / monthlySaved) / Math.log(1 + realInt);
+  // calcTime =
+  //   Math.log(1 + (goal * realInt) / monthlySaved) / Math.log(1 + realInt);
   //calcTime is the actual amount of time needed to reach the savings goal////
-  // calcTime =
-  //   Math.log(1 + (goal * realInt) / (monthlySaved + currentSaved)) /
-  //   Math.log(1 + realInt);
-  // trying the calc again based on this resource  https://www.thecalculatorsite.com/finance/calculators/compound-interest-formula
-  // calcTime =
-  //   Math.log(goal / (currentSaved + monthlySaved)) /
-  //   (12 * Math.log(1 + realInt / 12)) /
-  //   12;
-
+  calcTime =
+    Math.log(1 + (goal * realInt) / (monthlySaved + currentSaved)) /
+    Math.log(1 + realInt);
   console.log(`Months needed ${calcTime}`);
   // https://stackoverflow.com/questions/39275225/how-to-convert-a-number-of-months-into-months-and-years
   // act Years and actMonths are for formatting year month style
@@ -145,80 +116,7 @@ const calculate = () => {
 calculateButton.onclick = function () {
   calculate();
 };
-
-// adding JS to get the range track and ticks working
-///////////////////////////////////////////////////////////////////////
-// looking at using Javascript to generate slider marks
-// https://codepen.io/viestursm/pen/BayEjaN
-function init() {
-  // getting slider to pass into the functions that control appearance and behavior
-  // each range input has the class tick-slider-input
-  const sliders = document.getElementsByClassName("tick-slider-input");
-
-  // when the page loads use the default values to set up sliders
-  for (let slider of sliders) {
-    // add event listener
-    slider.oninput = onSliderInput;
-
-    updateProgress(slider);
-    // the slider is not in the onInput because it is defined on page load and doesnt change after that.
-    setTicks(slider);
-  }
-}
-// when range handle is moved pass in that event to update the slider
-function onSliderInput(event) {
-  //event.target is input id  name then append the class name #name.tick-slider-input
-  updateProgress(event.target);
-}
-
-// updateProgress used to display handle on track green vs black
-function updateProgress(slider) {
-  // progressId coming from "data-progress-id" data set on each range input
-  let progress = document.getElementById(slider.dataset.progressId);
-  // percent is the percent handle has traveled
-  const percent = getSliderPercent(slider);
-  // giving input via data-progress-id style
-  progress.style.width = percent * 100 + "%";
-}
-// this is getting the position of the handle relative to its range
-function getSliderPercent(slider) {
-  //slider AKA event.target.value
-  // console.log(event.target.value);
-  // slider.max and min are coming from range input min and max
-  const range = slider.max - slider.min;
-  // slider.value is comming from intial value or what value is when handle dragged
-  const absValue = slider.value - slider.min;
-  // console.log(absValue / range);
-  // returning the percent of the way the handle has traveled
-  // accross the slider
-  return absValue / range;
-}
-
-function setTicks(slider) {
-  // console.log(slider);
-  // get the data-tick-id from each input range input
-  let container = document.getElementById(slider.dataset.tickId);
-  // console.log(container);
-  // get the data-tick-step value for each input range
-  const spacing = parseFloat(slider.dataset.tickStep);
-  // console.log(spacing);
-  // get the min and max from the range input and do the math
-  const sliderRange = slider.max - slider.min;
-  // console.log(sliderRange);
-  // starting from 0
-  const tickCount = sliderRange / spacing + 1; // +1 to account for 0
-  // console.log(tickCount);
-  for (let i = 0; i < tickCount; i++) {
-    // create a spans with class tick-slider-tick
-    let tick = document.createElement("span");
-    tick.className = "tick-slider-tick";
-    // console.log(tick);
-    container.appendChild(tick);
-  }
-}
-
-window.onload = init;
-
-// ////////////////
 //TODO work on formatting to match example site, look at DYI jumbotron
 // TODO for the chart will need to know given plan if goal will be reached and compare that to what the actual plan (monthly savings) need to be to achieve it. In the given amount of time
+
+r;
