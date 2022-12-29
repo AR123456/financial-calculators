3b973663380842aeeaf29670abdcfe36b7d090fe
AR123456 <coloradoz99@gmail.com>
2022-12-29 08:02:57 -0700

    calcs

--- Savings-Goal-Calculator/app.js
+++ Savings-Goal-Calculator/app.js
@@ -149,7 +149,37 @@ const calculate = () => {
   // pv becomes the result of +calcFV(rate, 12, pmt, pv, 1)
   // next loop run +calcFV(rate, 12, pmt, pv+lastFV, 1)
   // Would a switch case work for this  ?///////////////////////////
+  for (let index = 0; index < years; index++) {
+    let j = pv;
+    j = calcFV(rate, 12, pmt, j[index.value], type);
+    newPV.push(j);
 
+    // let FV2 = calcFV(rate, 12, pmt, newPV[0], type);
+    // newPV.push(FV2);
+
+    // let FV3 = calcFV(rate, 12, pmt, newPV[1], type);
+    // newPV.push(FV3);
+
+    // let FV4 = calcFV(rate, 12, pmt, newPV[2], type);
+    // newPV.push(FV4);
+
+    // let FV5 = calcFV(rate, 12, pmt, newPV[3], type);
+    // newPV.push(FV5);
+
+    // let FV6 = calcFV(rate, 12, pmt, newPV[4], type);
+    // newPV.push(FV6);
+
+    // let FV7 = calcFV(rate, 12, pmt, newPV[5], type);
+    // newPV.push(FV7);
+
+    // let FV8 = calcFV(rate, 12, pmt, newPV[6], type);
+    // newPV.push(FV8);
+    // let FV9 = calcFV(rate, 12, pmt, newPV[7], type);
+    // newPV.push(FV9);
+    // let FV10 = calcFV(rate, 12, pmt, newPV[8], type);
+    // newPV.push(FV10);
+    console.log("newPV", newPV);
+  }
   // let FV1 = calcFV(rate, 12, pmt, pv, type);
   // newPV.push(FV1);
 
