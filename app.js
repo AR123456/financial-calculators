086e753e9d3bc882d2f0cc463c3d40d0ed1be7ab
AR123456 <coloradoz99@gmail.com>
2023-01-23 05:16:02 -0700

    work on chart

--- app.js
+++ app.js
@@ -189,6 +189,70 @@ const calculate = () => {
   `;
   chartDiv.innerHTML = `<span>    Actual monthly savings according to plan vs needed monthly saving to
   get to goal in plan years.<span>`;
+  // ///////////////////
+  // create teh bootstrap responsive table container
+  const cont = document.createElement("div");
+  cont.classList.add("table-responsive", "container");
+  // create the main table element
+  const tbl = document.createElement("table");
+  tbl.classList.add(
+    "table",
+    "table-striped",
+    "table-hover",
+    "table-bordered",
+    "table-sm",
+    "caption-top"
+  );
+  tbl.innerHTML = `<caption class="text-center">By Year</caption>`;
+  // create the parts of the table
+  const tblBody = document.createElement("tbody");
+  const tblHeader = document.createElement("thead");
+  const tblFooter = document.createElement("tfoot");
+  //  add to elements
+  tblHeader.innerHTML = `
+  <tr><th class="text-center">Result Summary</th></tr>`;
+  tblBody.innerHTML = `
+  <tr>  
+  <td class="text-end">Savings goal </td>
+  <td class="text-end"> ${goal}</td>
+  </tr>
+  <tr>  
+  <td class="text-end">Target years to save </td>
+  <td class="text-end"> ${years}</td>
+   </tr>
+  <tr>  
+  <td class="text-end">Amount currently saved </td> 
+  <td class="text-end"> ${currentSaved}</td> 
+   </tr>
+  <tr>  
+  <td class="text-end">Expected rate of return</td>
+   <td class="text-end"> ${rate}</td>
+    </tr>
+  <tr>  
+  <td class="text-end">Inflation rate  </td>
+    <td class="text-end"> ${expectedInflation}</td>
+    </tr>   
+   `;
+  tblFooter.innerHTML = `
+  <tr> 
+    <td class="text-end">Total after 10 years </td>
+    <td class="text-end"> ${growthByYearNeededToBeSaved[10]}</td>
+  </tr>
+  <tr>    
+    <td class="text-end">Amount required to meet goal in ${years} years</td>
+    <td class="text-end"> ${Ppmt} monthly</td>
+  </tr> `;
+  // put tblHeader into the table
+  tbl.appendChild(tblHeader);
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // put footer into table
+  tbl.appendChild(tblFooter);
+  cont.appendChild(tbl);
+  // appends <table> to div >
+  tableDiv.appendChild(cont);
+
+  // /////////////
 };
 
 calculateButton.onclick = function () {
@@ -391,70 +455,7 @@ function clearPriorTable() {
   tableDiv.innerHTML = "";
 }
 function generateTables() {
-  clearPriorTable();
+  // clearPriorTable();
   tableDiv.scrollIntoView();
-  generateResultSummary();
-}
-function generateResultSummary() {
-  // create teh bootstrap responsive table container
-  const cont = document.createElement("div");
-  cont.classList.add("table-responsive", "container");
-  // create the main table element
-  const tbl = document.createElement("table");
-  tbl.classList.add(
-    "table",
-    "table-striped",
-    "table-hover",
-    "table-bordered",
-    "table-sm",
-    "caption-top"
-  );
-  tbl.innerHTML = `<caption class="text-center">By Year</caption>`;
-  // create the parts of the table
-  const tblBody = document.createElement("tbody");
-  const tblHeader = document.createElement("thead");
-  const tblFooter = document.createElement("tfoot");
-  //  add to elements
-  tblHeader.innerHTML = `
-    <tr><th class="text-center">Result Summary</th></tr>`;
-  tblBody.innerHTML = `
-    <tr>  
-    <td class="text-end">Savings goal </td>
-    <td class="text-end"> ${"goal"}</td>
-    </tr>
-    <tr>  
-    <td class="text-end">Target years to save </td>
-    <td class="text-end"> ${"years"}</td>
-     </tr>
-    <tr>  
-    <td class="text-end">Amount currently saved </td> 
-    <td class="text-end"> ${"currentSaved"}</td> 
-     </tr>
-    <tr>  
-    <td class="text-end">Expected rate of return</td>
-     <td class="text-end"> ${"rate"}</td>
-      </tr>
-    <tr>  
-    <td class="text-end">Inflation rate  </td>
-      <td class="text-end"> ${"expectedInflation"}</td>
-      </tr>   
-     `;
-  tblFooter.innerHTML = `
-    <tr> 
-      <td class="text-end">Total after 10 years </td>
-      <td class="text-end"> ${growthByYearNeededToBeSaved[10]}</td>
-    </tr>
-    <tr>    
-      <td class="text-end">Amount required to meet goal in ${"years"} years</td>
-      <td class="text-end"> ${"ppmt"} monthly</td>
-    </tr> `;
-  // put tblHeader into the table
-  tbl.appendChild(tblHeader);
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // put footer into table
-  tbl.appendChild(tblFooter);
-  cont.appendChild(tbl);
-  // appends <table> to div >
-  tableDiv.appendChild(cont);
+  // generateResultSummary();
 }
