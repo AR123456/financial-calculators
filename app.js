50f0a735c4397aa771f132e568ccfe986dcec75a
AR123456 <coloradoz99@gmail.com>
2023-01-24 04:44:45 -0700

    second table duplicates

--- app.js
+++ app.js
@@ -194,8 +194,7 @@ const calculate = () => {
   chartDiv.innerHTML = `<span>    Actual monthly savings according to plan vs needed monthly saving to
   get to goal in plan years.<span>`;
   // ///////////////////
-
-  // create The bootstrap responsive table container
+  // create The bootstrap responsive table container for summary report
   const cont = document.createElement("div");
   cont.classList.add("table-responsive", "container");
   // create the main table element
@@ -256,6 +255,62 @@ const calculate = () => {
   cont.appendChild(tbl);
   // appends <table> to div >
   tableDiv.appendChild(cont);
+  ////
+  // create THe  bootstrap responsive table container for the table report
+  const contB = document.createElement("div");
+  contB.classList.add("table-responsive", "container");
+  contB.id = "tableDiv";
+  // creates a <table> element and a <tbody> element
+  const tblB = document.createElement("table");
+  // add classes in this case for bootstrap 5
+  tblB.classList.add(
+    "table",
+    "table-striped",
+    "table-hover",
+    "table-bordered",
+    "table-sm",
+    "caption-top"
+  );
+  tblB.innerHTML = `<caption class="text-center">Summary</caption>`;
+  const tblBodyB = document.createElement("tbody");
+  const tblHeaderB = document.createElement("thead");
+  // const row = document.createElement("tr");
+  const trFirstB = document.createElement("tr");
+  tblHeaderB.innerHTML = `
+                <tr>  
+                <th class="text-center">Years ${years}</th>
+                <th class="text-center">Contributing ${monthlySaved}</th>
+                <th class="text-center">Contributing ${Ppmt} to get to goal</th> 
+                </tr>`;
+  trFirstB.innerHTML = `<tr>  
+                <td></td>
+                <td class="text-end">$${growthByYear[0]} Starting Balance</td>
+                <td class="text-end">$${growthByYearNeededToBeSaved[0]} Starting Balance</td> 
+                </tr>`;
+  // creating all cells
+  for (let i = 1; i < yearsToGrow.length; i++) {
+    // creates a table row
+    const rowB = document.createElement("tr");
+    let trsB = `<tr>  
+                  <td class="text-end">${yearsToGrow[i]}</td>
+                  <td class="text-end">$ ${growthByYear[i]}</td>
+                  <td class="text-end">$ ${growthByYearNeededToBeSaved[i]}</td> 
+                  </tr>`;
+    rowB.innerHTML = trsB;
+    // prepend adds to front append adds to end
+    tblBodyB.prepend(trFirstB);
+    // add the row to the end of the table body
+    tblBodyB.appendChild(rowB);
+  }
+  // put tblHeader into the table
+  tblB.appendChild(tblHeaderB);
+  // put the <tbody> in the <table>
+  tblB.appendChild(tblBodyB);
+  // put the table into the table responsive container for bootstrap
+  contB.appendChild(tblB);
+  // appends <table> to the hard coded div >
+  dynamicGenerateTable.appendChild(contB);
+  //////// end of calc function
 };
 
 calculateButton.onclick = function () {
