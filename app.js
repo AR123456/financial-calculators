ee8422bce98328c4fb8fab0d1f5037d2da0c8122
AR123456 <coloradoz99@gmail.com>
2023-01-30 05:04:49 -0700

    done decommented

--- app.js
+++ app.js
@@ -46,15 +46,11 @@ inputGoal.addEventListener("keyup", () => {
   formatCurrency(inputGoal.value);
 });
 function syncInputs() {
-  // https://stackoverflow.com/questions/64199456/changing-the-value-of-the-range-slider-and-input-box-at-the-same-time
-
-  // /////////////////////////
   sliderGoal.addEventListener("input", function () {
     inputGoal.value = sliderGoal.value;
   });
   inputGoal.addEventListener("input", function () {
     sliderGoal.value = inputGoal.value;
-    // console.log(sliderGoal.dataset);
     updateProgress(sliderGoal);
   });
   sliderYears.addEventListener("input", function () {
@@ -81,7 +77,6 @@ function syncInputs() {
   sliderExpectedRate.addEventListener("input", function () {
     inputExpectedRate.value = sliderExpectedRate.value;
     let DisplayExpectedRate = (sliderExpectedRate.value / 12).toFixed(2);
-    console.log(DisplayExpectedRate);
     displayExpectedRate.innerHTML = `<p>MPR ${DisplayExpectedRate}%</p>`;
   });
   inputExpectedRate.addEventListener("input", function () {
@@ -102,7 +97,7 @@ function syncInputs() {
 syncInputs();
 
 displayExpectedRate.innerHTML = `<p>(MPR ${DisplayExpectedRate}%)</p>`;
-// calculate  function
+// Main calculate  function
 const calculate = () => {
   const goal = parseFloat(inputGoal.value);
   const years = parseFloat(inputYears.value);
@@ -111,28 +106,22 @@ const calculate = () => {
   const expectedReturn = parseFloat(inputExpectedRate.value);
   const expectedInflation = parseFloat(inputInflationRate.value);
   const DisplayRate = (expectedReturn / 12).toFixed(2);
-
   // Assuming compounding monthly payment at ebd of month- type 0
   // nper - The total number of payment periods.
   const nper = years * 12;
-  console.log(nper);
   //expected as a decimal
   const APR = expectedReturn / 100;
   //The interest rate per period aka monthly rate of return
   const rate = APR / nper;
-  console.log(rate);
   //payment made each period.
   const pmt = monthlySaved;
-  console.log(pmt);
   // The present value
   const pv = currentSaved;
-  console.log(pv);
   // 1 for pymt at beginning of month, 0 at end
   const type = 0;
   const fv = goal;
   /////////////////////////// FV
   const FV = calcFV(rate, nper, pmt, pv, type);
-  console.log(`The calculated future value ${FV}`);
   /////////////// Array of FV from start to end of goal period year by year
   growthByYear.push(pv);
   let newPV = calcFV(rate, 12, pmt, pv, type);
@@ -141,20 +130,14 @@ const calculate = () => {
     newPV = calcFV(rate, 12, pmt, newPV, type);
     growthByYear.push(newPV);
   }
-  console.log(growthByYear);
-  /// push goal years to years to grow array
   for (let i = 0; i <= years; i++) {
     yearsToGrow.push(i);
   }
-  console.log(yearsToGrow);
-
   //////////////////////////////////////PMT monthly actually needed to get to goal
   const PandIPmt = PMT(rate, nper, pv, fv, type);
-  console.log("PMT so monthly savings plus monthly interest ", PandIPmt);
   /////////////////////////////////////// IPMT what is int of monthly actually needed
   const ipmt = IPMT(pv, pmt, rate, nper);
-  console.log("part of monthly that is interest", ipmt);
-  //Show user needed monthly contribution
+  //Show user needed monthly contribution- not part that is intrest
   const Ppmt = parseFloat(
     PMT(rate, nper, pv, fv, type) - IPMT(pv, pmt, rate, nper)
   ).toFixed(2);
@@ -166,22 +149,12 @@ const calculate = () => {
     newPVP = calcFV(rate, 12, Ppmt, newPVP, type);
     growthByYearNeededToBeSaved.push(newPVP);
   }
-  console.log(growthByYearNeededToBeSaved);
   /////////////////////////////////////////////// NPER// number of periods - in months
   const calcTime = NPER(rate, pmt, pv, goal, type);
   // https://stackoverflow.com/questions/39275225/how-to-convert-a-number-of-months-into-months-and-years
   // act Years and actMonths are for formatting year month style
   const actYears = Math.floor(calcTime / 12);
-  // let actMonths = Math.ceil(calcTime % 12);
   const actMonths = Math.round(calcTime % 12);
-  console.log(
-    "CalcTime",
-    calcTime,
-    "actYears",
-    actYears,
-    ",actMonths",
-    actMonths
-  );
   // Message to user based on calculations
   if (years > 1) {
     actualTime.innerHTML = ` <span> After ${years} years you will have $${FV}</span><br>
@@ -212,7 +185,7 @@ const calculate = () => {
   get to goal in plan years.<span>`;
   summaryTable();
   byYearTable();
-  // create The bootstrap responsive table container for summary report
+  // dynamically create The bootstrap responsive table container for summary report
   function summaryTable() {
     const cont = document.createElement("div");
     cont.classList.add("table-responsive", "container");
@@ -226,11 +199,9 @@ const calculate = () => {
       "caption-top"
     );
     tbl.innerHTML = `<caption class="text-center">Summary</caption>`;
-    // create the parts of the table
     const tblBody = document.createElement("tbody");
     const tblHeader = document.createElement("thead");
     const tblFooter = document.createElement("tfoot");
-    //  add to elements
     tblHeader.innerHTML = `
     <tr><th class="text-center">Result Summary</th></tr>`;
     tblBody.innerHTML = `
@@ -264,26 +235,19 @@ const calculate = () => {
       <td class="text-end">Amount required to meet goal in ${years} years</td>
       <td class="text-end"> ${formatCurrency(Ppmt)} monthly</td>
     </tr> `;
-    // put tblHeader into the table
     tbl.appendChild(tblHeader);
-    // put the <tbody> in the <table>
     tbl.appendChild(tblBody);
-    // put footer into table
     tbl.appendChild(tblFooter);
     cont.appendChild(tbl);
-    // appends <table> to div >
     tableDiv.appendChild(cont);
   }
 
   function byYearTable() {
-    // create The  bootstrap responsive table container for the by year report
-
+    // dynamically create The  bootstrap responsive table container for the by year report
     const contB = document.createElement("div");
     contB.classList.add("table-responsive", "container");
     contB.id = "tableDiv";
-    // creates a <table> element and a <tbody> element
     const tblB = document.createElement("table");
-    // add classes in this case for bootstrap 5
     tblB.classList.add(
       "table",
       "table-striped",
@@ -295,7 +259,6 @@ const calculate = () => {
     tblB.innerHTML = `<caption class="text-center">By Year</caption>`;
     const tblBodyB = document.createElement("tbody");
     const tblHeaderB = document.createElement("thead");
-    // const row = document.createElement("tr");
     const trFirstB = document.createElement("tr");
     tblHeaderB.innerHTML = `
                     <tr>  
@@ -316,9 +279,7 @@ const calculate = () => {
                       growthByYearNeededToBeSaved[0]
                     )} Starting Balance</td> 
                     </tr>`;
-    // creating all cells
     for (let i = 1; i < yearsToGrow.length; i++) {
-      // creates a table row
       const rowB = document.createElement("tr");
       let trsB = `<tr>  
                       <td class="text-end">${yearsToGrow[i]}</td>
@@ -330,21 +291,14 @@ const calculate = () => {
                       )}</td> 
                       </tr>`;
       rowB.innerHTML = trsB;
-      // prepend adds to front append adds to end
       tblBodyB.prepend(trFirstB);
-      // add the row to the end of the table body
       tblBodyB.appendChild(rowB);
     }
-    // put tblHeader into the table
     tblB.appendChild(tblHeaderB);
-    // put the <tbody> in the <table>
     tblB.appendChild(tblBodyB);
-    // put the table into the table responsive container for bootstrap
     contB.appendChild(tblB);
-    // appends <table> to the hard coded div >
     dynamicGenerateTable.appendChild(contB);
   }
-
   //////// end of calc function
 };
 
@@ -358,88 +312,46 @@ calculateButton.onclick = function () {
   scrollToChart();
 };
 
-// TODO put in its own file adding JS to get the range track and ticks working
+//JS to get the range track and ticks
 ///////////////////////////////////////////////////////////////////////
-// looking at using Javascript to generate slider marks
-// https://codepen.io/viestursm/pen/BayEjaN
 function initRangeSlider() {
-  // getting slider to pass into the functions that control appearance and behavior
-  // each range input has the class tick-slider-input
   const sliders = document.getElementsByClassName("tick-slider-input");
-
-  // when the page loads use the default values to set up sliders
   for (let slider of sliders) {
-    // add event listener
     slider.oninput = onSliderInput;
-
     updateProgress(slider);
-    // the slider is not in the onInput because it is defined on page load and doesnt change after that.
     setTicks(slider);
   }
 }
-// when range handle is moved pass in that event to update the slider
 function onSliderInput(event) {
-  //event.target is input id  name then append the class name #name.tick-slider-input
   updateProgress(event.target);
 }
-
-// updateProgress used to display handle on track green vs black
 function updateProgress(slider) {
-  // progressId coming from "data-progress-id" data set on each range input
   let progress = document.getElementById(slider.dataset.progressId);
-  // percent is the percent handle has traveled
   const percent = getSliderPercent(slider);
-  // giving input via data-progress-id style
   progress.style.width = percent * 100 + "%";
 }
-// this is getting the position of the handle relative to its range
 function getSliderPercent(slider) {
-  //slider AKA event.target.value
-  // console.log(event.target.value);
-  // slider.max and min are coming from range input min and max
   const range = slider.max - slider.min;
-  // slider.value is comming from intial value or what value is when handle dragged
   const absValue = slider.value - slider.min;
-  // console.log(absValue / range);
-  // returning the percent of the way the handle has traveled
-  // accross the slider
   return absValue / range;
 }
 
 function setTicks(slider) {
-  // console.log(slider);
-  // get the data-tick-id from each input range input
   let container = document.getElementById(slider.dataset.tickId);
-  // console.log(container);
-  // get the data-tick-step value for each input range
   const spacing = parseFloat(slider.dataset.tickStep);
-  // console.log(spacing);
-  // get the min and max from the range input and do the math
   const sliderRange = slider.max - slider.min;
-  // console.log(sliderRange);
-  // starting from 0
   const tickCount = sliderRange / spacing + 1; // +1 to account for 0
-  // console.log(tickCount);
   for (let i = 0; i < tickCount; i++) {
-    // create a spans with class tick-slider-tick
     let tick = document.createElement("span");
     tick.className = "tick-slider-tick";
-    // console.log(tick);
     container.appendChild(tick);
   }
 }
 
 window.onload = initRangeSlider;
-
 // CHART BOILER PLATE
-
-// https://www.chartjs.org/docs/latest/getting-started/usage.html
 const ctx = document.getElementById("myChart").getContext("2d");
 const displayChart = () => {
-  // call this after calculate
-  // const ctx = document.getElementById("myChart").getContext("2d");
-  // https://stackoverflow.com/questions/69559728/eror-chart-min-js13-uncaught-in-promise-error-canvas-is-already-in-use-cha
-  // https://www.youtube.com/watch?v=TvkT6a17L2s
   let chartStatus = Chart.getChart("myChart");
 
   if (chartStatus != undefined) {
@@ -448,22 +360,11 @@ const displayChart = () => {
   myChart = new Chart(ctx, {
     type: "bar",
     data: {
-      // labels: data.map((row) => row.year),
-      //  years for each year create a label
       labels: yearsToGrow,
-      // loop number of goal years and push do data array for each year what the actual savings are in blue, the first number
-      // and then amount needed per year to get to goal  each year the green number
       datasets: [
         {
-          // TODO use JS to show  monthlySaved
           label: "Plan monthly $",
-          // thousands of dollars scaled to goal
           data: growthByYear,
-          // data: [
-          //   1000, 1000, 7011, 1890, 13036, 2783, 19077, 3676, 25133, 4573, 31204,
-          //   5472, 37290, 6373, 43391, 7276, 49508, 8182, 55640, 9090, 61787,
-          //   10000,
-          // ],
           backgroundColor: [
             "rgba(47, 162, 47, 0.2)",
             "rgba(47, 162, 47, 0.2)",
@@ -473,8 +374,6 @@ const displayChart = () => {
             "rgba(47, 162, 47, 0.2)",
             "rgba(47, 162, 47, 0.2)",
             "rgba(47, 162, 47, 0.2)",
-
-            //  site default green rgba(47, 162, 47, 1)
           ],
           borderColor: [
             "rgba(54, 162, 235, 1)",
@@ -488,20 +387,12 @@ const displayChart = () => {
             "rgba(54, 162, 235, 1)",
 
             "rgba(54, 162, 235, 1)",
-            // "rgba(75, 192, 192, 1)",
           ],
           borderWidth: 1,
         },
         {
-          // TODO use JS to show  monthlySaved
           label: "Needed monthly $",
-          // thousands of dollars scaled to goal
           data: growthByYearNeededToBeSaved,
-          // data: [
-          //   1000, 1000, 7011, 1890, 13036, 2783, 19077, 3676, 25133, 4573, 31204,
-          //   5472, 37290, 6373, 43391, 7276, 49508, 8182, 55640, 9090, 61787,
-          //   10000,
-          // ],
           backgroundColor: [
             "rgba(54, 162, 235, 1)",
 
@@ -514,8 +405,6 @@ const displayChart = () => {
             "rgba(54, 162, 235, 1)",
 
             "rgba(54, 162, 235, 1)",
-
-            //  site default green rgba(47, 162, 47, 1)
           ],
           borderColor: [
             "rgba(47, 162, 47, 0.2)",
@@ -526,7 +415,6 @@ const displayChart = () => {
             "rgba(47, 162, 47, 0.2)",
             "rgba(47, 162, 47, 0.2)",
             "rgba(47, 162, 47, 0.2)",
-            // "rgba(75, 192, 192, 1)",
           ],
           borderWidth: 1,
         },
@@ -541,7 +429,6 @@ const displayChart = () => {
     },
   });
 };
-
 // ////////////////////////////// getting report on page ////////////
 viewReportButton.addEventListener("click", generateTables);
 function clearPriorTable() {
@@ -550,7 +437,6 @@ function clearPriorTable() {
 }
 function generateTables() {
   tableDiv.scrollIntoView();
-  // generateResultSummary();
 }
 function scrollToPlan() {
   backToTop.scrollIntoView();
--- calculations.js
+++ calculations.js
@@ -1,11 +1,4 @@
-//  this is the FV formula from stack overflow
-// https://stackoverflow.com/questions/1780645/how-to-calculate-future-value-fv-using-javascript
-// The FV function is a financial function that returns the future value of an investment
-// rate - The interest rate per period.
-// nper - The total number of payment periods.
-// pmt - The payment made each period. Must be entered as a negative number.
-// pv - [optional] The present value of future payments. If omitted, assumed to be zero. Must be entered as a negative number.
-// type - [optional] When payments are due. 0 = end of period, 1 = beginning of period. Default is 0.
+// Solve for  FV function
 export function calcFV(rate, nper, pmt, pv, type) {
   var pow = Math.pow(1 + rate, nper),
     fv;
@@ -16,13 +9,7 @@ export function calcFV(rate, nper, pmt, pv, type) {
   }
   return fv.toFixed(2);
 }
-/////// solving for PMT to determine what is acctually needed to get to goal
-// The PMT function is a financial function that returns the periodic payment for a loan
-// rate - The interest rate for the loan.
-// nper - The total number of payments for the loan. (in months)
-// pv - The present value, or total value of all loan payments now.(current saved )
-// fv -The future value, (goal)
-// type - [optional] When payments are due. 0 = end of period. 1 = beginning of period. Default is 0.
+/////// solving for PMT to determine needed to get to goal
 export function PMT(rate, nper, pv, fv, type) {
   if (!fv) {
     fv = 0;
@@ -34,9 +21,8 @@ export function PMT(rate, nper, pv, fv, type) {
   if (rate == 0) {
     return -(pv + fv) / nper;
   }
-  // The present value interest factor (PVIF) is a formula used to estimate the current worth of a sum of money that is to be received at some future date.
+  // Solve for  present value interest factor (PVIF)
   const pvif = Math.pow(1 + rate, nper);
-  console.log("this is pvif", pvif);
   let pmt = (-rate / (pvif - 1)) * (pv * pvif + fv) * -1;
 
   if (type == 1) {
@@ -46,35 +32,20 @@ export function PMT(rate, nper, pv, fv, type) {
   return pmt;
 }
 
-///////// IPMT
-// rate - intrest rate per period
-//per - the period for which you want to find the interest and must be in the range 1 to Nper
-//nper - total number of payment periods of the investment
-// pv - the present value of the lump sum amount that a series of future payemtns is worth now
-// fv - the future value, or a cash balance you want to attain after the last payment is made if omitted =0
+/////////Solve for IPMT
 export function IPMT(pv, pmt, rate, per) {
   var tmp = Math.pow(1 + rate, per);
   let ipmt = 0 - (pv * tmp * rate + pmt * (tmp - 1)) * -1;
   return ipmt;
 }
 
-// stack overflow of the NPER excel function number  of periods  https://gist.github.com/Nitin-Daddikar/43899765e30274ec739f44ebbac434c3
-// solve the annuity for n
-// rate - The interest rate per period.
-// pmt - payment The payment made each period. since this is savings make negative number
-// pv - present The present value, or total value of all payments now. For savings making this a neg number for the calc
-// goal - future - [optional] The future value, or a cash balance you want after the last payment is made. Defaults to 0.
-// type - [optional] When payments are due. 0 = end of period. 1 = beginning of period. Default is 0.
+// solve for  NPER
 
 let future;
 export function NPER(rate, pmt, pv, goal, type) {
-  // Initialize type
   type = typeof type === "undefined" ? 0 : type;
-  // Initialize future value
   future = typeof future === "undefined" ? 0 : goal;
-  // Return number of periods
   const num = -pmt * (1 + rate * type) - goal * rate;
-  // this is savings, not loan so making pv negative so the calc works
   const den = -pv * rate + -pmt * (1 + rate * type);
   return Math.log(num / den) / Math.log(1 + rate);
 }
